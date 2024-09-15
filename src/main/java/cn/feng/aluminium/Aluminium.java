532169d1b69e04b050239979f6dd898e51701d21
Feng Cheng <ChengF3ng233@outlook.com>
2024-09-15 23:19:12 +0800

    Initial commit

---
+++ src/main/java/cn/feng/aluminium/Aluminium.java
@@ -0,0 +1,12 @@
+package cn.feng.aluminium;
+
+/**
+ * @author ChengFeng
+ * @since 2024/9/15
+ **/
+public class Aluminium {
+    public static final Aluminium INSTANCE = new Aluminium();
+    public void init() {
+
+    }
+}
